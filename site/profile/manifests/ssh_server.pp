c8960d5586464a4a2ce6c181ab0e27c8fcb90eb1
leegalan <30468022+leegalan@users.noreply.github.com>
2022-11-16 14:46:16 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCf2KiY/Xylzhhh2EtiErkv+rlKpoRDpiG0cNwsbQhyUNnO9o2ZTotXYONgz/3dVoxvCz1JTqOzXuBy/HAIlmRbgUZJAp4R3WNzfS3eVcuojTG9i8D3fcXiWxH5F3n2211S0Ru+PxfJ5PZ1Xz308ZDJcu2uxMXU9XszvxrW2IqTeoHwNCN1aHM4bbF+8HXYcnWc/anKHnugFGfCXJuaQspfS9PPIIEflFQom3jF2N2nTkGj+6ICWTeDSMYkTd+iJl8Qg5Ne4Dwxo+LlaCQZJSs71konTO2Qj+R1ruuhqCXlxt1ZV4KhWdny2EMV88tUKS2B9eA1gRtwZrGjAyx3sxyN',
 	}  
 }
